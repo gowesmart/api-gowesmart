722851e45bc7dcc625a0f0dd97264c03c768faa5
raihanmd <mraihanf471@gmail.com>
2024-08-02 10:09:01 +0700

    chore: change jwt utils

--- model/entity/roles_entity.go
+++ model/entity/roles_entity.go
@@ -2,6 +2,8 @@ package entity
 
 import "time"
 
+var IDRoleAdmin = 1
+
 type Role struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement"`
 	Name      string    `gorm:"type:varchar(20);not null;unique"`
