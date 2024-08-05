e880f4b51e6eddd3ad53df5348c85f0832e80b6a
Usman Lubis <usmanlubis11@gmail.com>
2024-08-05 19:55:21 +0700

    feat(TASK001):add category and bike model and migration

---
+++ model/entity/bike_entity.go
@@ -0,0 +1,20 @@
+package entity
+
+import "time"
+
+type Bike struct {
+	ID          uint       `gorm:"primaryKey;autoIncrement"`
+	CategoryID  uint       `gorm:"not null"`
+	Name        string     `gorm:"unique;not null;type:varchar(50)"`
+	Brand       string     `gorm:"not null;type:varchar(20)"`
+	Description string     `gorm:"type:varchar(1000)"`
+	Year        int        `gorm:"not null"`
+	Price       int        `gorm:"not null"`
+	ImageUrl    string     `gorm:"type:varchar(255)"`
+	Stock       int        `gorm:"not null"`
+	IsAvailable bool       `gorm:"not null;default:true"`
+	CreatedAt   time.Time  
+	UpdatedAt   time.Time  
+	Category    Category   `gorm:"foreignKey:CategoryID"`
+	Review      []Review   `gorm:"references:ID"`
+}
