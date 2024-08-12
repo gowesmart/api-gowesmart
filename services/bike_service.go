705fbb235eea0967475660184ae510d0ba7c91d2
raihanmd <mraihanf471@gmail.com>
2024-08-12 13:59:58 +0700

    fix: remove filtering stock in get all bikes

--- services/bike_service.go
+++ services/bike_service.go
@@ -139,7 +139,6 @@ func (service *BikeService) GetAllBikes(c *gin.Context, bikeQueryReq *request.Bi
 	var bikes []response.BikeResponse
 
 	query := db.Model(&entity.Bike{}).
-		Where("stock > 0").
 		Select("id, category_id, name, brand, description, year, price, image_url, stock, is_available, created_at, updated_at")
 
 	if bikeQueryReq.CategoryID != 0 {
